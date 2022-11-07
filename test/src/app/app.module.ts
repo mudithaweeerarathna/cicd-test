98724acde4b98ced2fcd58200d68ea5fe2d46536
Muditha Weerarathna <mmweerarathna123@gmail.com>
2022-11-07 15:22:31 +0530

    test1

--- test/src/app/app.module.ts
+++ test/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BranchComponent } from './branch/branch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BranchComponent
   ],
   imports: [
     BrowserModule,
